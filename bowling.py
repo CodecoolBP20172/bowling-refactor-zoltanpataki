043ebdaee236bbc002e82f64b8c480ff34ca55bf
Pataki Zoltan <zolee7777@gmail.com>
2017-07-24 19:03:56 +0200

    the first part of the for loop is rearranged and shortened

--- bowling.py
+++ bowling.py
@@ -3,19 +3,16 @@ def score(game):
     frame = 1
     first_roll = True
     for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+        if game[i] == '/' and frame < 10:
+            result += 10 - last + get_value(game[i+1])
         else:
             result += get_value(game[i])
-        if frame < 10 and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
+        if game[i].upper() == "X" and frame < 10:
+            result += get_value(game[i+1])
+            if game[i+2] == '/':
+                result += 10 - get_value(game[i+1])
+            else:
+                result += get_value(game[i+2])
         last = get_value(game[i])
         if not first_roll:
             frame += 1
