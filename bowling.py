2d6c313868bc6708ddb33df62da5ab7d01195e30
Pataki Zoltan <zolee7777@gmail.com>
2017-07-24 17:33:08 +0200

    in_first_half name is changed to first_roll and pep8 styling in score function

--- bowling.py
+++ bowling.py
@@ -1,13 +1,13 @@
 def score(game):
     result = 0
     frame = 1
-    in_first_half = True
+    first_roll = True
     for i in range(len(game)):
         if game[i] == '/':
             result += 10 - last
         else:
             result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
@@ -17,14 +17,14 @@ def score(game):
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
-        if not in_first_half:
+        if not first_roll:
             frame += 1
-        if in_first_half == True:
-            in_first_half = False
+        if first_roll is True:
+            first_roll = False
         else:
-            in_first_half = True
+            first_roll = True
         if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
+            first_roll = True
             frame += 1
     return result
 
